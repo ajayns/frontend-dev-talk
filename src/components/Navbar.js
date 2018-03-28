db5a0420d90e26597e77bf691357ad1cadc1537f
ajayns <ajay.ns@outlook.com>
2018-03-29 00:50:57 +0530

    Add menu functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Menu } from 'semantic-ui-react'
 
-const Navbar = (subreddit, handleMenuChange, handleRefreshClick) => (
+const Navbar = ({selectedSub, handleMenuChange, handleRefreshClick}) => (
     <div style={{ margin: '1rem' }}>
         <Menu pointing secondary>
-            <Menu.Item name='react' active={subreddit === 'react'} onClick={handleItemClick} />
-            <Menu.Item name='frontend' active={subreddit === 'frontend'} onClick={handleItemClick} />
-            <Menu.Item name='webdev' active={subreddit === 'webdev'} onClick={handleItemClick} />
-            <Menu.Item name='javascript' active={subreddit === 'javascript'} onClick={handleItemClick} />
-            <Menu.Item name='open source' active={subreddit === 'opensource'} onClick={handleItemClick} />
-            <Menu.Item name='programming' active={subreddit === 'programming'} onClick={handleItemClick} />
-            <Menu.Item name='learn javascript' active={subreddit === 'learnjavascript'} onClick={handleItemClick} />
+            <Menu.Item name='reactjs' active={selectedSub === 'reactjs'} onClick={handleMenuChange} />
+            <Menu.Item name='frontend' active={selectedSub === 'frontend'} onClick={handleMenuChange} />
+            <Menu.Item name='webdev' active={selectedSub === 'webdev'} onClick={handleMenuChange} />
+            <Menu.Item name='javascript' active={selectedSub === 'javascript'} onClick={handleMenuChange} />
+            <Menu.Item name='open source' active={selectedSub === 'opensource'} onClick={handleMenuChange} />
+            <Menu.Item name='programming' active={selectedSub === 'programming'} onClick={handleMenuChange} />
+            <Menu.Item name='learn javascript' active={selectedSub === 'learnjavascript'} onClick={handleMenuChange} />
             <Menu.Menu position='right'>
                 <Menu.Item name='refresh' onClick={() => console.log('refresh')} />
                 <Menu.Item name='info' onClick={() => console.log('author')} />
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+// Import actions
+import {
+    selectSubreddit,
+    fetchPostsIfNeeded,
+    invalidateSubreddit
+} from '../actions'
 
 import Navbar from '../components/Navbar'
 
-export default class App extends Component {
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.handleMenuChange = this.handleMenuChange.bind(this)
+    }
+
+    handleMenuChange(e, {name}) {
+        console.log(name)
+        this.props.dispatch(selectSubreddit(name.replace(/ /g, '')))
+    }
+
+    handleRefreshClick() {
+        console.log('refresh clicked')
+    }
+
     render() {
+        const { selectedSubreddit } = this.props
+
         return (
             <div>
-                <Navbar />
+                <Navbar
+                    selectedSub={selectedSubreddit} 
+                    handleMenuChange={this.handleMenuChange}
+                    handleRefreshClick={this.handleRefreshClick}
+                />
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    const { selectedSubreddit } = state
+
+    return {
+        selectedSubreddit
+    }
+}
+
+export default connect(mapStateToProps)(App)
