941393b70afc64cc1b5bfbf506ccb600c2d736fc
ajayns <ajay.ns@outlook.com>
2018-03-29 00:02:27 +0530

    Update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Menu } from 'semantic-ui-react'
 
-export default class Navbar extends Component {
-    state = { activeItem: 'react' }
+const Navbar = (subreddit, handleMenuChange, handleRefreshClick) => (
+    <div style={{ margin: '1rem' }}>
+        <Menu pointing secondary>
+            <Menu.Item name='react' active={subreddit === 'react'} onClick={handleItemClick} />
+            <Menu.Item name='frontend' active={subreddit === 'frontend'} onClick={handleItemClick} />
+            <Menu.Item name='webdev' active={subreddit === 'webdev'} onClick={handleItemClick} />
+            <Menu.Item name='javascript' active={subreddit === 'javascript'} onClick={handleItemClick} />
+            <Menu.Item name='open source' active={subreddit === 'opensource'} onClick={handleItemClick} />
+            <Menu.Item name='programming' active={subreddit === 'programming'} onClick={handleItemClick} />
+            <Menu.Item name='learn javascript' active={subreddit === 'learnjavascript'} onClick={handleItemClick} />
+            <Menu.Menu position='right'>
+                <Menu.Item name='refresh' onClick={() => console.log('refresh')} />
+                <Menu.Item name='info' onClick={() => console.log('author')} />
+            </Menu.Menu>
+        </Menu>
+    </div>
+)
 
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-    render() {
-        const { activeItem } = this.state
-
-        return (
-            <div style={{ margin: '1rem' }}>
-                <Menu pointing secondary>
-                    <Menu.Item name='react' active={activeItem === 'react'} onClick={this.handleItemClick} />
-                    <Menu.Item name='frontend' active={activeItem === 'frontend'} onClick={this.handleItemClick} />
-                    <Menu.Item name='webdev' active={activeItem === 'webdev'} onClick={this.handleItemClick} />
-                    <Menu.Item name='javascript' active={activeItem === 'javascript'} onClick={this.handleItemClick} />
-                    <Menu.Item name='open source' active={activeItem === 'open source'} onClick={this.handleItemClick} />
-                    <Menu.Item name='programming' active={activeItem === 'programming'} onClick={this.handleItemClick} />
-                    <Menu.Item name='learn javascript' active={activeItem === 'learn javascript'} onClick={this.handleItemClick} />
-                    <Menu.Menu position='right'>
-                        <Menu.Item name='refresh' active={activeItem === 'refresh'} onClick={this.handleItemClick} />
-                        <Menu.Item name='info' active={activeItem === 'info'} onClick={() => console.log('author')} />
-                    </Menu.Menu>
-                </Menu>
-            </div>
-        )
-    }
-}
+export default Navbar
