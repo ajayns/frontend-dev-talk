0463d4d571cf931447ae233d5522c0b8af2a3fae
ajayns <ajay.ns@outlook.com>
2018-03-30 11:26:20 +0530

    Fix innerHTML issue

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import { Card, Image } from 'semantic-ui-react'
+import Remarkable from 'remarkable'
 
-const Post = ({ url, title, author, selftext, thumbnail }) => (    
+const getMarkup = (text) => {
+    if (text) {
+        const md = new Remarkable()
+        return { __html: md.render(text)}
+    } else {
+        return { __html: ``}
+    }    
+}
+
+const notImage = [ 'self', 'default' ]
+
+const Post = ({ url, title, author, selftext, thumbnail }) => (
     <Card href={url} style={{width: '100%'}}>
         <Card.Content>
-            {thumbnail !== "self" && <Image floated='right' src={thumbnail} />}
+            { !notImage.includes(thumbnail) && <Image floated='left' src={thumbnail} rounded bordered size="tiny" />}
             <Card.Header>{title}</Card.Header>
             <Card.Meta>by {author}</Card.Meta>
-            <Card.Description>{selftext}</Card.Description>
+            <Card.Description dangerouslySetInnerHTML={getMarkup(selftext)} />
         </Card.Content>
     </Card>
 )
